86fe716c0f82d8e32947987e2b13003b385f8e27
Alexander Szabados <alex.szabados@bepic.tv>
2024-11-17 10:45:14 +0100

    add dataloader

---
+++ code/dataloader.py
@@ -0,0 +1,33 @@
+from torch.utils.data import DataLoader, Dataset, random_split, Subset
+from PIL import Image
+import os
+
+class PneumoniaDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Gather all image paths and labels
+        self.image_paths = []
+        self.labels = []
+        for class_name in os.listdir(root_dir):
+            class_path = os.path.join(root_dir, class_name)
+            if os.path.isdir(class_path):
+                label = 1 if class_name == "PNEUMONIA" else 0
+                for img_name in os.listdir(class_path):
+                    if img_name.endswith(('.jpg', '.jpeg', '.png')):  # Check valid image files
+                        self.image_paths.append(os.path.join(class_path, img_name))
+                        self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        img_path = self.image_paths[index]
+        img = Image.open(img_path).convert('RGB')  # Ensure RGB format
+        label = self.labels[index]
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label

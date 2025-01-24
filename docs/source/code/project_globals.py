490bc7bf3def958b11d098e92f244f79e55977b9
Alexander Szabados <alex.szabados@bepic.tv>
2025-01-24 15:47:58 +0100

    upgrade PYTHONPATH

--- docs/source/code/project_globals.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-DATADIR = pathlib.Path('../data/raw')
-TRAIN_DIR = DATADIR / 'train'
-VAL_DIR = DATADIR / 'val'
-TEST_DIR = DATADIR / 'test'
-CHECKPOINT_DIR = DATADIR / 'checkpoints'
